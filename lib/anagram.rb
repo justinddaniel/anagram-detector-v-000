5d75e7de2644fce3b4d1fd2ae3f4e441a64b10d3
Justin Daniel <justinddaniel@gmail.com>
2017-09-26 19:47:37 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,12 @@ class Anagram
   end
 
   def match(array)
+    word = self.word
+    array.each do |w|
+      delete_if w.length != word.length
+    end
+    array.keep_if {|w| }
+      
   end
 
 end
