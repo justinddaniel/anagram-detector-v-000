2f5059ff10dbc3ed32c4d6b49055f0088c9265c4
Justin Daniel <justinddaniel@gmail.com>
2017-09-26 19:57:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,10 @@ class Anagram
     array.each do |w|
       delete_if w.length != word.length
     end
-    array.keep_if {|w| }
-      
+    array.keep_if {|w| w.chars.permutation.scan(word).length > 0 }
+    array 
+  end
+
   end
 
 end
