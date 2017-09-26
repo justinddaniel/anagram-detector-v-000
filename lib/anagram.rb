9f36a1797bb94e14413b1197ccd8f4063554e6a3
Justin Daniel <justinddaniel@gmail.com>
2017-09-26 20:28:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
 
   def match(array)
     word = self.word
-    array.delete_if {w.length != word.length}
+    array.delete_if {|w| w.length != word.length}
     array.keep_if {|w| w.chars.to_a.permutation.map(&:join).any?{|ana| ana == word}}
     array
   end
